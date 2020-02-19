8167db57e1a414983181f0b54a97e90c45ee0819
Rogerio Silva <dgelask8@gmail.com>
2020-02-19 19:57:11 -0300

    Sistema de login unico 2

--- php-avancado-b7web/OO/projeto_loginunico/config.php
+++ php-avancado-b7web/OO/projeto_loginunico/config.php
@@ -1,6 +1,6 @@
 <?php
 try {
-	$pdo = new PDO("mysql:dbname=projeto_loginunico;host=localhost", "root", "root");
+	$pdo = new PDO("mysql:dbname=projeto_loginunico;host=localhost", "root", "");
 } catch(PDOException $e) {
 	echo "ERRO: ".$e->getMessage();
 	exit;
