2fae52648844e576f33656488600056b1e6f760f
Rogerio <dgelask8@gmail.com>
2020-05-25 16:06:20 -0300

    Orientação a Objetos: Entendendo Autoload Aula 14

---
+++ b7web/php-nivel1/modulo05-orientacao-a-objetos/aula14-entendendo-autoload/autoloader.php
@@ -0,0 +1,7 @@
+<?php
+
+spl_autoload_register(function ($class){
+    if(file_exists('classes/' . $class . '.php')){
+        require_once 'classes/' . $class . '.php';
+    }
+});
---
+++ b7web/php-nivel1/modulo05-orientacao-a-objetos/aula14-entendendo-autoload/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once 'autoloader.php';
+
+$m = new Matematica();
+echo $m->somar(20,9);
