d3492b024602d0a61975dab61ac1b40430459394
Rogerio <dgelask8@gmail.com>
2020-05-27 17:55:34 -0300

    Banco de Dados, CRUD, DAO, SOLID: Conectando ao Banco de Dados com PDO Aula 01

--- b7web/php-nivel1/modulo06-banco-de-dados-crud-dao-solid/aula01-conectando-ao-banco-de-dados-com-pdo/index.php
+++ b7web/php-nivel1/modulo06-banco-de-dados-crud-dao-solid/aula01-conectando-ao-banco-de-dados-com-pdo/index.php
@@ -5,4 +5,6 @@ $pdo = new PDO("mysql:dbname=teste;host=localhost", "root","");
 $sql = $pdo->query("SELECT * FROM usuarios");
 $dados = $sql->fetchAll(PDO::FETCH_ASSOC);
 
+echo "Numero de registros: " . $sql->rowCount();
+
 var_dump($dados);
