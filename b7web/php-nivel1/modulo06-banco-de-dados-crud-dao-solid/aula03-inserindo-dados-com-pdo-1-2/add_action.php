1991f67eae2a165745d48a65f544d097eea346f0
Rogerio <dgelask8@gmail.com>
2020-05-28 17:44:45 -0300

    Banco de Dados, CRUD, DAO, SOLID: Inserindo dados com PDO 1/2 Aula 03

---
+++ b7web/php-nivel1/modulo06-banco-de-dados-crud-dao-solid/aula03-inserindo-dados-com-pdo-1-2/add_action.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'config.php';
+
+$nome = filter_input(INPUT_POST, 'name');
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDADE_EMAIL);
+
+if($name and $email){
+
+    $add_user = $pdo->prepare("INSERT INTO usuarios (nome, senha) VALUES (:nome, :email)");
+    $add_user->bindValue(':nome', $nome);
+    $add_user->bindValue(':email', $email, PDO::PARAM_STR);
+    $add_user->execute();
+
+    header('Location: index.php');
+    exit();
+
+}else{
+    header('Location: add_user.php');
+    exit();
+}
+
---
+++ b7web/php-nivel1/modulo06-banco-de-dados-crud-dao-solid/aula03-inserindo-dados-com-pdo-1-2/add_user.php
@@ -0,0 +1,15 @@
+<form method="post" action="add_action.php">
+    <label>Nome</label><br>
+    <input type="text" name="name">
+
+    <br>
+    <br>
+
+    <label>E-mail</label><br>
+    <input type="email" name="email">
+
+    <br>
+    <br>
+
+    <input type="submit" value="Adicionar">
+</form>
