07847c5824acf6310123c37c74e24b2b3f82a0be
Rogerio <dgelask8@gmail.com>
2020-05-28 17:05:45 -0300

    Banco de Dados, CRUD, DAO, SOLID: Conceito CRUD Aula 02

---
+++ b7web/php-nivel1/modulo06-banco-de-dados-crud-dao-solid/aula02-conceito-crud/add_user.php
@@ -0,0 +1,3 @@
+<form action="add_action.php">
+
+</form>
---
+++ b7web/php-nivel1/modulo06-banco-de-dados-crud-dao-solid/aula02-conceito-crud/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'config.php';
+?>
+
+<a href="add_user.php">Add User</a>
+
+<table border="1" width="100%">
+    <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th>Ações</th>
+    </tr>
+</table>
