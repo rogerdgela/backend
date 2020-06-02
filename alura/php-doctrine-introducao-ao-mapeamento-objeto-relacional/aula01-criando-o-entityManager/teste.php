9d2614bb63784add308ed6708d593506efdb841f
Rogerio <dgelask8@gmail.com>
2020-06-02 11:55:06 -0300

    PHP Doctrine: Introdução ao Mapeamento Objeto-Relacional - Criando o EntityManager Aula 01

---
+++ alura/php-doctrine-introducao-ao-mapeamento-objeto-relacional/aula01-criando-o-entityManager/teste.php
@@ -0,0 +1,10 @@
+<?php
+
+use Alura\Doctrine\Helper\EntityManagerFactory;
+
+require_once __DIR__ . "vendor/autoload.php";
+
+$entityManagerfactory = new EntityManagerFactory();
+$entityManager = $entityManagerfactory->getEntityManager();
+
+var_dump($entityManager->getConnection());
