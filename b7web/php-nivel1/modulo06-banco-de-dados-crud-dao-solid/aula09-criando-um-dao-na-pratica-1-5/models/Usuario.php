78b4e000bfd3d30a9e23e97bd8fdb5514d637948
Rogerio Silva <dgelask8@gmail.com>
2020-06-02 19:59:55 -0300

    Banco de Dados, CRUD, DAO, SOLID: Delete: Criando um DAO na prática (1/5) Aula 09

---
+++ b7web/php-nivel1/modulo06-banco-de-dados-crud-dao-solid/aula09-criando-um-dao-na-pratica-1-5/models/Usuario.php
@@ -0,0 +1,48 @@
+<?php
+
+
+class Usuario
+{
+    private $id;
+    private $nome;
+    private $email;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id): void
+    {
+        $this->id = trim($id);
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome): void
+    {
+        $this->nome = ucwords(trim($nome));
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email): void
+    {
+        $this->email = strtolower(trim($email));
+    }
+}
+
+interface UsuarioDao
+{
+    public function add(Usuario $u);
+    public function findAll();
+    public function findById($id);
+    public function update(Usuario $u);
+    public function delete($id);
+}
