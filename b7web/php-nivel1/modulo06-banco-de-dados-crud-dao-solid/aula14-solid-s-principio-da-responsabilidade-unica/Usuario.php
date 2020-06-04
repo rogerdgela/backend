a535aedaa4a878efcafb82defec0b66b921089c3
Rogerio Silva <dgelask8@gmail.com>
2020-06-04 20:25:32 -0300

    Banco de Dados, CRUD, DAO, SOLID: SOLID: S - Princípio da Responsabilidade Única Aula 13

---
+++ b7web/php-nivel1/modulo06-banco-de-dados-crud-dao-solid/aula14-solid-s-principio-da-responsabilidade-unica/Usuario.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class Usuario
+{
+    private $name;
+    private $email;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+}
---
+++ b7web/php-nivel1/modulo06-banco-de-dados-crud-dao-solid/aula14-solid-s-principio-da-responsabilidade-unica/UsuarioBd.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class UsuarioBd
+{
+    public function add(Usuario $u){}
+
+    public function update(Usuario $u){}
+
+    public function delete($id){}
+}
