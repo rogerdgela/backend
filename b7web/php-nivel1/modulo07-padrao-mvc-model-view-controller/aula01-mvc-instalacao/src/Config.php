b7b829eb2b6d2d721ecce6b95baa53aaca703ecf
Rogerio Silva <dgelask8@gmail.com>
2020-06-24 20:41:33 -0300

    Padrão MVC (Model, View, Controller): MVC: Instalação Aula 01

--- b7web/php-nivel1/modulo07-padrao-mvc-model-view-controller/aula01-mvc-instalacao/src/Config.php
+++ b7web/php-nivel1/modulo07-padrao-mvc-model-view-controller/aula01-mvc-instalacao/src/Config.php
@@ -2,7 +2,7 @@
 namespace src;
 
 class Config {
-    const BASE_DIR = 'cursos/backend/b7web/php-nivel1/modulo07-padrao-mvc-model-view-controller/aula01-mvc-instalacao/public';
+    const BASE_DIR = '/cursos/backend/b7web/php-nivel1/modulo07-padrao-mvc-model-view-controller/aula01-mvc-instalacao/public';
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
