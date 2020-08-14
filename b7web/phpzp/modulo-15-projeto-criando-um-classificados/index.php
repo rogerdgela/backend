50e28e6437dcfa2f875c20b50c1f309e7f8a13cc
Rogerio <dgelask8@gmail.com>
2020-08-14 08:55:12 -0300

    Módulo 15: Projeto - Criando um Classificados - Aula 02/11

---
+++ b7web/phpzp/modulo-15-projeto-criando-um-classificados/index.php
@@ -0,0 +1,19 @@
+<?php require_once 'pages/header.php'; ?>
+
+    <div class="container-fluid">
+         <div class="jumbotron">
+             <h2>Possuímos cerca de 999 anúncios.</h2>
+             <p>E mais de 10000 usuários cadastrados</p>
+         </div>
+
+        <div class="row">
+            <div class="col-sm-3">
+                <h4>Pesquisa Avançada</h4>
+            </div>
+            <div class="col-sm-9">
+                <h4>Últimos Anúncios</h4>
+            </div>
+        </div>
+    </div>
+
+<?php require_once 'pages/footer.php'; ?>
