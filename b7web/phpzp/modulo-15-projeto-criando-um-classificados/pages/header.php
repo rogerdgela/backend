b328a112e8d801de53df5adacc008d7f4c09317c
Rogerio <dgelask8@gmail.com>
2020-08-17 11:39:16 -0300

    Módulo 15: Projeto - Criando um Classificados - Aula 03/11

--- b7web/phpzp/modulo-15-projeto-criando-um-classificados/pages/header.php
+++ b7web/phpzp/modulo-15-projeto-criando-um-classificados/pages/header.php
@@ -1,3 +1,4 @@
+<?php require_once 'config.php' ?>
 <html>
 <head>
     <title>Classificados</title>
@@ -15,8 +16,13 @@
         </div>
 
         <ul class="nav navbar-nav navbar-right">
-            <li><a href="">Cadastre-se</a></li>
-            <li><a href="">Login</a></li>
+            <?php if(isset($_SESSION['cLogin']) && !empty($_SESSION['cLogin'])){ ?>
+                <li><a href="meus-anuncio.php">Meu Anúncios</a></li>
+                <li><a href="sair.php">Sair</a></li>
+            <?php }else{ ?>
+                <li><a href="cadastre-se.php">Cadastre-se</a></li>
+                <li><a href="login.php">Login</a></li>
+            <?php } ?>
         </ul>
     </div>
 </nav>
