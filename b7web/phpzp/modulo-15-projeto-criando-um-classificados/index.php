e325acecc849a5f66ace56b508600fd0e3d73dbf
Rogerio <dgelask8@gmail.com>
2020-08-24 14:45:09 -0300

    Módulo 15: Projeto - Criando um Classificados - Aula 12 - EXTRA

--- b7web/phpzp/modulo-15-projeto-criando-um-classificados/index.php
+++ b7web/phpzp/modulo-15-projeto-criando-um-classificados/index.php
@@ -113,7 +113,11 @@
                 </table>
                 <ul class="pagination">
                     <?php for ($q = 1; $q <= $total_paginas; $q++){ ?>
-                        <li class="<?= ($p == $q) ? 'active' : ''; ?>"><a href="index.php?p=<?= $q; ?>"><?= $q; ?></a></li>
+                        <li class="<?= ($p == $q) ? 'active' : ''; ?>"><a href="index.php?<?php
+                            $w = $_GET;
+                            $w['p'] = $q;
+                            echo http_build_query($w);
+                        ?>"><?= $q; ?></a></li>
                     <?php } ?>
                 </ul>
             </div>
