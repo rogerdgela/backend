55a77d4bf08802e2d76539c3fb642f313f5d27f6
Rogerio <dgelask8@gmail.com>
2020-08-28 11:38:03 -0300

    Módulo 16: PHP Super Avançado - Estrutura MVC: Autoload + Core (2/2) - Aula 05

---
+++ b7web/phpzp/modulo-16-php-super-avancado/aula05-estrutura-mvc-autoload-core-2-2/controllers/GaleriaController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class GaleriaController
+{
+    public function index()
+    {
+        echo "Listando as galerias";
+    }
+
+    public function abrir($id)
+    {
+        echo "Abrindo galeria ".$id;
+    }
+}
