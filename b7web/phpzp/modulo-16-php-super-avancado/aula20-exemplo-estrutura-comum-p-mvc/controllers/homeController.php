0632bc20fb55f429b1a1d66762d4b68752d077ea
Rogerio <dgelask8@gmail.com>
2020-09-15 16:17:39 -0300

    Módulo 16: PHP Super Avançado - Exemplo: Estrutura Comum p/ MVC - Aula 20

---
+++ b7web/phpzp/modulo-16-php-super-avancado/aula20-exemplo-estrutura-comum-p-mvc/controllers/homeController.php
@@ -0,0 +1,17 @@
+<?php
+class homeController extends controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        $dados = array();
+        
+        $posts = new Posts();
+        $dados['posts'] = $posts->getPosts(10);        
+        
+        $this->loadTemplate('home', $dados);
+    }
+
+}
