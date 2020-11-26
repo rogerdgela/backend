02c73b9267d7a3cdc0436a4781cbf995aa8a6ecd
Rogerio Silva <dgelask8@gmail.com>
2020-11-25 21:55:26 -0300

    Módulo 17 PHP JEDI: Modificando o MVC para PSR-4 - Aula 19

---
+++ b7web/phpzp/modulo-17-php-jedi/aula20-modificando-o-mvc-para-psr-4/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+namespace Controllers;
+
+use \Core\Controller;
+use \Models\Usuarios;
+
+class HomeController extends Controller
+{
+
+	public function index() {
+		$array = array();
+
+		$usuarios = new Usuarios();
+		$array['lista'] = $usuarios->getAll();
+
+		$this->loadTemplate('home', $array);
+	}
+
+}
