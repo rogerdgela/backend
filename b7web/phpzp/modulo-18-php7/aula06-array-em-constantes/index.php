7081f72fdcfdcc6ee6da24e0f114b50d77086b71
Rogerio Silva <dgelask8@gmail.com>
2020-12-03 20:28:08 -0300

    Módulo 18 PHP 7: Array em constantes - Aula 06

---
+++ b7web/phpzp/modulo-18-php7/aula06-array-em-constantes/index.php
@@ -0,0 +1,14 @@
+<?php
+
+define('CONFIG',[
+   'name' => 'Nome do Sistema',
+   'version' => '1.6'
+]);
+
+echo CONFIG['name'] . ' Versão ' . CONFIG['version'];
+
+/*define('CONFIG_DBNAME','banco');
+define('CONFIG_USER','root');
+define('CONFIG_PASS','root');
+
+echo CONFIG_DBNAME;*/
