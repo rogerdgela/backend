c640965d86ca31802e46b38010dcaea7f4bebc03
Rogerio Silva <dgelask8@gmail.com>
2020-12-09 21:09:26 -0300

    Módulo 19 PHP Orientado a Testes: Testando erros de PHP - Aula 05

---
+++ b7web/phpzp/modulo-19-php-orientado-a-testes/aula05-testando-erros-de-php/tests/ArquivoTest.php
@@ -0,0 +1,20 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ArquivoTest extends TestCase
+{
+    public function testInclude()
+    {
+        $this->assertTrue(
+            file_exists('config.php')
+        );
+    }
+
+    /*
+     * @expectedException PHPUnit\Framework\Error\Error
+     */
+    /*public function testFalhaNoInlude()
+    {
+        include 'config.php';
+    }*/
+}
