f3763f7fd43e5e26094c8345cd6913bfda88906a
Rogerio Silva <dgelask8@gmail.com>
2021-02-04 20:15:47 -0300

    Teste de backup do banco

--- b7web/phpzp/modulo-20-php-e-segurança-blindando-aplicacoes-web/aula09-backup-automatico-do-banco/index.php
+++ b7web/phpzp/modulo-20-php-e-segurança-blindando-aplicacoes-web/aula09-backup-automatico-do-banco/index.php
@@ -1,2 +1,3 @@
 <?php
-exec("C:/wamp64/bin/mysql/mysql8.0.18/bin/mysqldump -u root devsbook > devsbookk-".date("d-m").".sql");
+system("C:/wamp64/bin/mysql/mysql8.0.18/bin/mysqldump -u root devsbook > devsbook-".date("d-m-Y").".sql");
+//exec("C:/wamp64/bin/mysql/mysql8.0.18/bin/mysqldump -u root devsbook > devsbookk-".date("d-m").".sql");
