8022ae7b2852e3ba24a0b158c07ab133986d4d2b
Rogerio Silva <dgelask8@gmail.com>
2021-02-09 20:24:25 -0300

    Módulo 20: PHP & Segurança: Blindando Aplicações Web - Ataque XSS - Aula 10

---
+++ b7web/phpzp/modulo-20-php-e-segurança-blindando-aplicacoes-web/aula10-ataque-xss/busca.php
@@ -0,0 +1,1 @@
+<h1>Voce pesquisou por: <?= htmlspecialchars($_GET['busca']) ?></h1>
---
+++ b7web/phpzp/modulo-20-php-e-segurança-blindando-aplicacoes-web/aula10-ataque-xss/index.php
@@ -0,0 +1,4 @@
+<form method="get" action="busca.php">
+    <input type="text" name="busca">
+    <input type="submit" value="Pesquisar">
+</form>
