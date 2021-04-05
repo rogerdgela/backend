e19d8a5f1b801697d1ccedffc41d5c94a858a946
Rogerio Silva <dgelask8@gmail.com>
2021-04-05 20:29:36 -0300

    Módulo 21: WebServices - Devstagram API: Endpoint users/feed (3/3) - Aula 21

--- b7web/phpzp/modulo-21-webServices/aula08-mvc-psr4-webservice-devstagram/Models/Photos.php
+++ b7web/phpzp/modulo-21-webServices/aula08-mvc-psr4-webservice-devstagram/Models/Photos.php
@@ -53,10 +53,42 @@ class Photos extends Model
     				$array[$k]['name'] = $user_info['name'];
 					$array[$k]['avatar'] = $user_info['avatar'];
 				    $array[$k]['url'] = BASE_URL.'media/photos/'.$item['url'];
+
+				    $array[$k]['like_count'] = $this->getLikeCount($item['id']);
+				    $array[$k]['comments'] = $this->getComments($item['id']);
 			    }
 		    }
 	    }
 
     	return $array;
     }
+
+    public function getLikeCount($id_photo)
+    {
+	    $sql = "SELECT COUNT(*) as c FROM photos_likes WHERE id_photo = :id";
+	    $sql = $this->db->prepare($sql);
+	    $sql->bindValue(':id', $id_photo);
+	    $sql->execute();
+
+	    $info = $sql->fetch(\PDO::FETCH_ASSOC);
+
+	    return $info['c'];
+    }
+
+	public function getComments($id_photo)
+	{
+		$array = [];
+		$sql = "SELECT photos_comments.*, users.name FROM photos_comments
+				LEFT JOIN users ON users.id = photos_comments.id_user
+    			WHERE photos_comments.id_photo = :id";
+		$sql = $this->db->prepare($sql);
+		$sql->bindValue(':id', $id_photo);
+		$sql->execute();
+
+		if($sql->rowCount() > 0){
+			$array = $sql->fetchAll(\PDO::FETCH_ASSOC);
+		}
+
+		return $array;
+	}
 }
