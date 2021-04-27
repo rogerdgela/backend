cac59f61b663a11bb2ff3232b8e53d2fc0e8d5de
Rogerio Silva <dgelask8@gmail.com>
2021-04-27 20:12:45 -0300

    Módulo 22: Frameworks - Criando uma aplicação com Slim (3/3) - Aula 08

--- b7web/phpzp/modulo-22-frameworks/slim/src/classes/Lista.php
+++ b7web/phpzp/modulo-22-frameworks/slim/src/classes/Lista.php
@@ -24,4 +24,32 @@ class Lista {
         $sql->bindValue(':telefone', $data['telefone']);
         $sql->execute();
     }
+
+    public function getContato($id){
+        $array = [];
+
+        $sql = $this->db->prepare("SELECT * FROM lista WHERE id = :id");
+        $sql->bindValue(':id', $id);
+        $sql->execute();
+
+        if($sql->rowCount() > 0){
+            $array = $sql->fetch();
+        }
+
+        return $array;
+    }
+
+    public function update($data, $id){
+        $sql = $this->db->prepare("UPDATE lista SET nome = :nome, telefone = :telefone WHERE id = :id");
+        $sql->bindValue(':nome', $data['nome']);
+        $sql->bindValue(':telefone', $data['telefone']);
+        $sql->bindValue(':id', $id);
+        $sql->execute();
+    }
+
+    public function del($id){
+        $sql = $this->db->prepare("DELETE FROM lista WHERE id = :id");
+        $sql->bindValue(':id', $id);
+        $sql->execute();
+    }
 }
--- b7web/phpzp/modulo-22-frameworks/slim/src/routes.php
+++ b7web/phpzp/modulo-22-frameworks/slim/src/routes.php
@@ -9,8 +9,8 @@ return function (App $app) {
 
     $app->get('/', function (Request $request, Response $response, array $args) use ($container) {
         $lista = new Lista($this->db);
-
         $args['lista'] = $lista->getLista();
+
         return $container->get('renderer')->render($response, 'home.phtml', $args);
     });
 
@@ -20,11 +20,32 @@ return function (App $app) {
 
     $app->post('/add', function (Request $request, Response $response, array $args) use ($container){
         $data = $request->getParsedBody();
-
         $lista = new Lista($this->db);
         $lista->add($data);
 
         return $response->withStatus(302)->withHeader('Location','./');
+    });
+
+    $app->get('/edit/{id}', function (Request $request, Response $response, array $args) use ($container) {
+        $lista = new Lista($this->db);
+        $args['info'] = $lista->getContato($args['id']);
+
+        return $container->get('renderer')->render($response, 'edit.phtml', $args);
+    });
+
+    $app->post('/edit/{id}', function (Request $request, Response $response, array $args) use ($container){
+        $data = $request->getParsedBody();
+        $lista = new Lista($this->db);
+        $lista->update($data,$args['id']);
+
+        return $response->withStatus(302)->withHeader('Location','../');
+    });
+
+    $app->get('/del/{id}', function (Request $request, Response $response, array $args) use ($container) {
+        $lista = new Lista($this->db);
+        $args['info'] = $lista->del($args['id']);
+
+        return $response->withStatus(302)->withHeader('Location','../');
     });
 
     /*$app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
