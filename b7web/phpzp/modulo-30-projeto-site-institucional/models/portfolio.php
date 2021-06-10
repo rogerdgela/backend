29f7eff1d8ce1bda0f12f594a58f6051aeead715
Rogerio Silva <dgelask8@gmail.com>
2021-06-10 19:27:09 -0300

    Módulo 30: Projeto - Site Institucional - Criando um site institucional (2/3) - Aula 02

--- b7web/phpzp/modulo-30-projeto-site-institucional/models/portfolio.php
+++ b7web/phpzp/modulo-30-projeto-site-institucional/models/portfolio.php
@@ -1,21 +1,19 @@
 <?php
 class Portfolio extends model {
     
-    public function getTrabalhos($n='') {
-        $array = array();
+    public function getTrabalhos($n = null) {
+        $array = [];
         
-        $sql = "SELECT * FROM portfolio ";
+        $sql = "SELECT * FROM portfolio";
         
         if(!empty($n)) {
-            $sql .= "LIMIT ".$n;
+            $sql .= " LIMIT ".$n;
         }
         
         $sql = $this->db->query($sql);
         
         if($sql->rowCount() > 0) {
-            
             $array = $sql->fetchAll();
-            
         }
         
         return $array;
